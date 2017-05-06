2a37920a37eac8f40b8155316f29c0fdb4f1e52f
yasgamesdev <yasgamesdev@gmail.com>
2017-05-06 10:02:27 +0900

    chapter6

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -13,6 +13,23 @@ public class Ball : MonoBehaviour {
     {
         layerMask = LayerMask.GetMask("Bound");
         velocity = (transform.forward + transform.right) * speed;
+
+        CreateBlocks();
+    }
+
+    void CreateBlocks()
+    {
+        Transform parent = GameObject.Find("Blocks").transform;
+
+        Vector3 start_pos = new Vector3(-4.0f, 0, 5.0f);
+        for(int x=0; x<5; x++)
+        {
+            for (int z = 0; z < 5; z++)
+            {
+                GameObject block = (GameObject)Instantiate(Resources.Load("Block"), parent);
+                block.transform.position = start_pos + new Vector3(2.0f * x, 0, 1.0f * z);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -22,6 +39,8 @@ public class Ball : MonoBehaviour {
         {
             transform.position += (hitInfo.point - transform.position);
             velocity += -2.0f * Vector3.Dot(velocity, hitInfo.normal) * hitInfo.normal;
+
+            CheckBlockDestroy(hitInfo);
         }
         else
         {
@@ -29,6 +48,11 @@ public class Ball : MonoBehaviour {
         }
     }
 
-    void Update () {
-	}
+    void CheckBlockDestroy(RaycastHit hitInfo)
+    {
+        if(hitInfo.collider.tag == "Block")
+        {
+            Destroy(hitInfo.collider.gameObject);
+        }
+    }
 }
---
+++ Assets/Racket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+    [SerializeField]
+    float accel = 1000.0f;
+    Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rigid.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * accel,ForceMode.Impulse);
+    }
+}
